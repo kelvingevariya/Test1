bdbdfe2e01171c7b81f366b5acef0db057f9dc54
Kelvin <kelvin.zerotimesolutions@gmail.com>
2024-02-06 15:13:18 +0530

    changes after pr review

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
-import { Request, Response, response } from "express";
+import { Request, Response } from "express";
 import quizJSON from "./JSONData/quiz.json";
 import responseJSON from "./JSONData/response.json";
 import formatJSON from "./JSONData/format.json";
 import sectionJSON from "./JSONData/section.json"
+import { news, games, quiz, flight, newsContent, gamesContent, quizContent, flightContent } from "./types"
 
 
-import { json } from "stream/consumers";
+
 
 const express = require("express");
 
@@ -17,17 +18,17 @@ const PORT: number = 4000;
 
 app.use(express.json());
 
-app.get("/scores", (req: Request, res: Response) => {
-    const topic: [] = req.body.topic;
-    const format: [] = req.body.format;
-    const source: [] = req.body.source;
+app.post("/scores", (req: Request, res: Response) => {
+    const topic: number[] = req.body.topic;
+    const format: number[] = req.body.format;
+    const source: number[] = req.body.source;
 
 
 
     //* Adding the initial scores to all sections
     function calculatePoints() {
-        responseJSON.forEach((sec) => {
-            //@ts-ignore
+        responseJSON.forEach((sec: quiz | games | news | flight | any) => {
+
             sec.points = 0;
         });
 
@@ -35,27 +36,21 @@ app.get("/scores", (req: Request, res: Response) => {
         quizJSON.forEach((topicQuiz) => {
             let i = 0
             if (topicQuiz.topic_id === topic[i]) {
-                responseJSON.forEach((secName) => {
+                responseJSON.forEach((secName: quiz | any) => {
                     if (secName.section_type === "Quiz") {
-                        //@ts-ignore
                         secName.points += 100;
                     }
                 });
                 i++;
-
             }
-
         });
 
         for (let i = 0; i < topic.length; i++) {
-            //! Quiz in topic 
-            let pointsAddedQuiz = [];
 
             //! News in topic 
 
-            responseJSON.forEach((key) => {
+            responseJSON.forEach((key: news | any) => {
                 if (key.section_type === "news" && key.content.some(element => element.topic_id === topic[i])) {
-                    //@ts-ignore
                     key.points += 100;
                 }
             });
@@ -65,30 +60,26 @@ app.get("/scores", (req: Request, res: Response) => {
             formatJSON.forEach((element) => {
                 if (element.id === format[i]) {
                     let elementType = element.title;
-
                     //! Conditions for quiz
                     if (elementType === "Playing" || elementType === "Self-practicing") {
-                        responseJSON.forEach((secName) => {
+                        responseJSON.forEach((secName: quiz | any) => {
                             if (secName.section_type === "Quiz") {
-                                //@ts-ignore
                                 secName.points += 10;
                             }
                         });
                     }
                     //! Condition for news
                     if (elementType === "Reading") {
-                        responseJSON.forEach((secName) => {
+                        responseJSON.forEach((secName: news | any) => {
                             if (secName.section_type === "news") {
-                                //@ts-ignore
                                 secName.points += 10;
                             }
                         });
                     }
                     //! Condition for games
                     if (elementType === "Playing") {
-                        responseJSON.forEach((secName) => {
+                        responseJSON.forEach((secName: games | any) => {
                             if (secName.section_type === "games") {
-                                //@ts-ignore
                                 secName.points += 10;
                             }
                         });
@@ -98,48 +89,43 @@ app.get("/scores", (req: Request, res: Response) => {
 
             //! Customm Section  
 
-            responseJSON.forEach((sec) => {
+            responseJSON.forEach((sec: flight | any) => {
                 if (sec.section_type === "Flight") {
-                    let secContent = sec.content;
+                    let secContent: flightContent | any = sec.content;
                     secContent.forEach((secCon) => {
                         //!  Format 
                         if (secCon.format_id === format[i]) {
-                            //@ts-ignore
                             sec.points += 10;
                         }
                         // //!Source 
                         if (secCon.source_id === source[i]) {
-                            //@ts-ignore
                             sec.points += 10;
                         }
-
                     })
-
                 }
             })
 
             //! Topic 
 
             for (let j = 0; j < sectionJSON.length; j++) {
-                responseJSON.forEach((secName) => {
+                responseJSON.forEach((secName: flight | any) => {
                     if (secName.section_type === "Flight") {
                         sectionJSON.forEach((idFormat) => {
                             if (idFormat.id === secName.section_id) {
                                 if (idFormat.topic_id === topic[j]) {
-                                    //@ts-ignore
                                     secName.points += 100;
                                 }
                             }
                         })
                     }
                 })
-            }
+            }    //* For loop ends here for the topic
         }
     }
 
     calculatePoints();
-    //@ts-ignore
-    responseJSON.sort((x, y) => y.points - x.points)
+
+    responseJSON.sort((x: news | games | quiz | flight | any, y: news | games | quiz | flight | any) => y.points - x.points);
 
 
     res.json(responseJSON);
